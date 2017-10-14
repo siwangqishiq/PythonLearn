50a5851a5f3fc1166a52aadc974865abcbb43d37
siwangqishiq <525647740@qq.com>
2017-10-14 12:31:45 +0800

    if fetal

--- ifpy.py
+++ ifpy.py
@@ -8,3 +8,12 @@ else:
 name =  input("输入名字");
 
 print("姓名%s 年龄%s" %(age , name));#output multipline 
+
+a = 100
+
+if a>10 and a<=100:
+	print("1")
+elif a>100 and a<=200:
+	print("2")
+else:
+	print("3")
