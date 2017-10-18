2a1fb9fc7e9e0068fa2fb812bd0c8f17bc66dc7b
panyi <525647740@qq.com>
2017-10-18 22:25:28 +0800

    def function

---
+++ buddha.py
@@ -0,0 +1,33 @@
+def print_buddha():
+	print("I am buddha")
+
+
+def swap(a,b):
+    print("a = %d , b = %d"%(a,b))
+    print("="*50)
+    c = a
+    a = b
+    b = c
+    print("a = %d , b = %d"%(a,b))
+
+def print_trigle():
+    print("*")
+    print("**")
+    print("***")
+    print("****")
+
+print_buddha()
+
+a_val = 11
+b_val = 12
+
+swap(a_val , b_val)
+
+print("a = %d , b = %d"%(a_val,b_val))
+
+
+print_trigle();
+
+swap("123","321");
+
+
