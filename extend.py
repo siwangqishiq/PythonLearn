1ab0021e92a5e833f93701fbe75dc87366edba3b
siwangqishiq <525647740@qq.com>
2017-11-09 13:45:16 +0800

    python extend

---
+++ extend.py
@@ -0,0 +1,20 @@
+﻿import sys
+
+class Animal(object):
+	def __init__(self):
+		print("Animal init func")
+	
+	def say(self):
+		print("hi~~~")
+	
+class Dog(Animal):
+	def __init__(self):
+		super().__init__()
+		print("Dog init func")
+
+	def fuck(self):
+		print("Fuck")
+		
+wang = Dog()
+wang.say()
+wang.fuck()
