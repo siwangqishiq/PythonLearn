73c2f0ed65eed9961cd7d393a74eb1ed856f86af
siwangqishiq <525647740@qq.com>
2017-11-11 12:38:26 +0800

    单利2

---
+++ single_instance.py
@@ -0,0 +1,35 @@
+﻿import sys
+
+class Cat(object):
+	__instance = None
+	
+	def __new__(cls):
+			if Cat.__instance == None:
+				Cat.__instance = object.__new__(cls)
+			return Cat.__instance
+	
+	def __init__(self):
+		self.name = "小花"
+	
+	def set_cat_name(self , new_name):
+		if self.name != new_name :
+			self.name = new_name
+			
+	def miao(self):
+		print(self.name + "  Miao miao miao!")
+		
+cat1 = Cat()
+cat1.miao()
+
+cat2 = Cat()
+cat2.miao()
+
+cat1.set_cat_name("大黄猫")
+cat2.miao()
+
+print(id(cat1))
+print(id(cat2))
+
+
+		
+		
